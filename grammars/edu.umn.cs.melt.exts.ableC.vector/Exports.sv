96555210aacb3dc92f9f0583f5b772e43429bffb
Lucas Kramer <krame505@umn.edu>
2024-11-11 17:23:52 -0600

    Fix exports

--- grammars/edu.umn.cs.melt.exts.ableC.vector/Exports.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.vector/Exports.sv
@@ -1,9 +1,9 @@
 grammar edu:umn:cs:melt:exts:ableC:vector;
 
 exports edu:umn:cs:melt:exts:ableC:vector:concretesyntax;
+exports edu:umn:cs:melt:exts:ableC:vector:abstractsyntax;
 
 exports edu:umn:cs:melt:exts:ableC:string;
 exports edu:umn:cs:melt:exts:ableC:templating;
 exports edu:umn:cs:melt:exts:ableC:constructor;
-
-exports edu:umn:cs:melt:exts:ableC:vector:abstractsyntax;
+exports edu:umn:cs:melt:exts:ableC:templateConstructor;
